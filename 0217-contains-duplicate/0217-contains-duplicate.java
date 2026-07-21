491ae24788034ea0a620780a4b84b85991e6209f
Mridu Mallick <mridumallick101@gmail.com>
2026-07-21 15:44:46 +0530

    Time: 18 ms (53.36%), Space: 110.7 MB (5.01%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Method 2 : HashMap
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            if(map.containsKey(nums[i])) return true;
+            else map.put(nums[i], 1);
+        }
+        return false;
+    }
+    /*
+    // Method 2 : Sorting
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return false;
+    }
+    */
+}
